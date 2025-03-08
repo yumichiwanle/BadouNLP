79865a92e22a8b93ab500bdf6403d6db3904189d
yumichiwanle <z1692717990@163.com>
2025-03-08 15:44:15 +0800

    Create config_file.py

---
+++ 詹庆裕/week11/config_file.py
@@ -0,0 +1,11 @@
+Config = {
+    "num_workers": 6,
+    "batch_size": 64,
+    "epoch": 20,
+    "pretrain_model_path": "D:/tool/bert-base-chinese",
+    "max_length": 100,
+    "num_labels":21128,
+    "lr": 0.002,
+    "optim_type": "adam",
+    "hidden_size":768,
+}
